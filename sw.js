344c54afd4fc14eb5412a82340e7949176e8cd90
EnNot76 <enzo.notari@live.de>
2026-05-20 14:06:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,50 @@
+const CACHE_NAME = "enigma3-cache-v1";
+const OFFLINE_URLS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/enigma-192.png",
+  "./icons/enigma-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js",
+  "https://unpkg.com/html5-qrcode"
+];
 
+// install: precache risorse principali
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+// activate: pulizia cache vecchie
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch: risposta cache-first con fallback rete
+self.addEventListener("fetch", event => {
+  const request = event.request;
+  event.respondWith(
+    caches.match(request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(request).catch(() => {
+        // opzionale: offline fallback page
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
